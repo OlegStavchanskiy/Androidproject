ef767d7fec0a8b9020e7d774f7c5ef89221fff83
Oleg Stavchanskiy <olegswork@mail.ru>
2024-03-30 03:20:25 +0300

    Lab2Android

--- app/src/main/java/com/example/droidquest/MainActivity.kt
+++ app/src/main/java/com/example/droidquest/MainActivity.kt
@@ -3,31 +3,60 @@ package com.example.droidquest
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mTrueButton: Button
     private lateinit var mFalseButton: Button
+    private lateinit var mNextButton: ImageButton
+    private lateinit var mBackButton: ImageButton
+    private lateinit var mQuestionTextView: TextView
+    private val mQuestionBank = listOf(
+        Question(R.string.question_android, true),
+        Question(R.string.question_linear, false),
+        Question(R.string.question_service, false),
+        Question(R.string.question_res, true),
+        Question(R.string.question_manifest, true)
+    )
+    private var mCurrentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         mTrueButton = findViewById(R.id.true_button)
         mTrueButton.setOnClickListener {
-            Toast.makeText(
-                this, R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            ).show()
+            checkAnswer(true)
         }
         mFalseButton = findViewById(R.id.false_button)
         mFalseButton.setOnClickListener{
-            Toast.makeText(this, R.string.incorrect_toast,
-                Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
 
+        }
+        mQuestionTextView = findViewById(R.id.question_text_view)
+        mQuestionTextView.setOnClickListener{            if(mCurrentIndex+1<mQuestionBank.size){
+            mCurrentIndex = mCurrentIndex + 1
+            updateQuestion()}}
+        mNextButton = findViewById(R.id.next_button)
+        mNextButton.setOnClickListener {
+            if(mCurrentIndex+1<mQuestionBank.size){
+            mCurrentIndex = mCurrentIndex + 1
+            updateQuestion()}
+
+        }
+        updateQuestion()
+        mBackButton = findViewById(R.id.back_button)
+        mBackButton.setOnClickListener{
+            if(mCurrentIndex-1>=0){
+
+            mCurrentIndex = mCurrentIndex - 1
+            updateQuestion()}
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -35,5 +64,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
     }
+    private fun updateQuestion() {
+        val question = mQuestionBank[mCurrentIndex].textResId
+        mQuestionTextView.setText(question)
+    }
+    private fun checkAnswer(userPressedTrue: Boolean) {
+        val answerIsTrue = mQuestionBank[mCurrentIndex].answerTrue
+        val messageResId = if (userPressedTrue == answerIsTrue) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
 }
